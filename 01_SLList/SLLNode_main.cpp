41f4efd1d1f3bd0945402ebf4e035bf6726da2ab
Katelyn McCullough <kmccullough7245@floridapoly.edu>
2026-08-26 14:50:49 +0000

    08/26/26 Push

---
+++ 01_SLList/SLLNode_main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "SLNode.hpp"
+
+int main(void) {
+    // Create SLLNodes
+    SLLNode* n1 = new SLLNode(1);
+    SLLNode* n2 = new SLLNode(2);
+    SLLNode* n3 = new SLLNode(3);
+    SLLNode* n4 = new SLLNode(4);
+
+    // Link the nodes
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+
+    //Print the value of the first node
+    std::cout << n1->data << std::endl;
+
+    // Print the value of the second node
+    std::cout << n2->data << std::endl;
+    // or
+    std::cout << n1->next->data << std::endl;
+
+    // Iterate through nodes
+    SLLNode* cur = n1;
+    while (cur != nullptr) {
+        std::cout << cur->data << " "; // access the data of the current node
+    }
+ 
+    // Assignment Change the output to have arrows between numbers and no arrows at the end
+
+    return 0;
+}
