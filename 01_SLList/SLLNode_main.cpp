e0823e87c83b46b9380e5cafb71c520a72c46589
Katelyn McCullough <kmccullough7245@floridapoly.edu>
2026-08-26 23:16:46 +0000

    Mini Assignment

--- 01_SLList/SLLNode_main.cpp
+++ 01_SLList/SLLNode_main.cpp
@@ -14,20 +14,32 @@ int main(void) {
     n3->next = n4;
 
     //Print the value of the first node
-    std::cout << n1->data << std::endl;
+    //std::cout << n1->data << std::endl;
 
     // Print the value of the second node
-    std::cout << n2->data << std::endl;
+    //std::cout << n2->data << std::endl;
     // or
-    std::cout << n1->next->data << std::endl;
+    //std::cout << n1->next->data << std::endl;
 
     // Iterate through nodes
-    SLLNode* cur = n1;
-    while (cur != nullptr) {
-        std::cout << cur->data << " "; // access the data of the current node
-    }
+    // SLLNode* cur = n1;
+    // while (cur != nullptr) {
+    //     std::cout << cur->data << " "; // access the data of the current node
+    //     cur = cur->next;
+    // }
  
     // Assignment Change the output to have arrows between numbers and no arrows at the end
+    SLLNode* cur = n1;
+    while (cur != nullptr) {
+        if (cur->next != nullptr) {
+            std::cout << cur->data << "->"; // access the data of the current node
+        } else {
+            std::cout << cur->data << std::endl; // access the data of the current node
+        }
+        
+        cur = cur->next;
+    }
+
 
     return 0;
 }
